c30e2e8f9d5cf5a21dcd02810ba84dbd1dbce0f0
miriani <mirian.tsintsadze1992@gmail.com>
2022-05-05 13:56:42 +0400

    replace candys

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import blank from "../asset/images/blank.png";
 import blue from "../asset/images/blue-candy.png";
 import orange from "../asset/images/orange-candy.png";
@@ -7,8 +7,12 @@ import purple from "../asset/images/purple-candy.png";
 import red from "../asset/images/red-candy.png";
 import yellow from "../asset/images/yellow-candy.png";
 
+const BorardWidth = 8;
+
 const Board = () => {
   const [allCandy, setAllCandy] = useState([]);
+  const [draggedCandy, setDraggedCandy] = useState(null);
+  const [dropCandy, setDropCandy] = useState(null);
   const candys = [blue, orange, green, purple, red, yellow];
   const createBoard = () => {
     let allCandy = [];
@@ -17,6 +21,36 @@ const Board = () => {
       allCandy.push(candys[candy]);
     }
     return setAllCandy(allCandy);
+  };
+
+  const dragHandler = (e) => {
+    setDraggedCandy(e.target);
+  };
+  const onDrop = (e) => {
+    setDropCandy(e.target);
+  };
+  const afterDrag = () => {
+    const draggedCandyIndex = parseInt(draggedCandy.getAttribute("data-id"));
+    const dropCandyIndex = parseInt(dropCandy.getAttribute("data-id"));
+    let drag = allCandy[draggedCandyIndex];
+    const isBlank = drag === blank;
+
+    const validMoves = [
+      draggedCandyIndex + 1,
+      draggedCandyIndex - 1,
+      draggedCandyIndex + BorardWidth,
+      draggedCandyIndex - BorardWidth,
+    ];
+    const isValidMove = validMoves.includes(dropCandyIndex);
+    if (isValidMove && !isBlank) {
+      allCandy[draggedCandyIndex] = allCandy[dropCandyIndex];
+      allCandy[dropCandyIndex] = drag;
+      setDraggedCandy(null);
+      setDropCandy(null);
+    } else {
+      allCandy[draggedCandyIndex] = drag;
+      allCandy[dropCandyIndex] = allCandy[dropCandyIndex];
+    }
   };
   useEffect(() => {
     createBoard();
@@ -32,6 +66,13 @@ const Board = () => {
             src={candy}
             alt="candy"
             className="img"
+            draggable={true}
+            onDragStart={dragHandler}
+            onDragEnter={(e) => e.preventDefault()}
+            onDragOver={(e) => e.preventDefault()}
+            onDragLeave={(e) => e.preventDefault()}
+            onDrop={onDrop}
+            onDragEnd={afterDrag}
           />
         );
       })}
